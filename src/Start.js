63038f4f1221007c0adc2f6fc0d6426c8995f63d
DanielTreep <danieltreep@live.nl>
2023-01-09 14:37:04 +0100

    setup

---
+++ src/Start.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import nfl from './images/nfl.png';
+
+export default function Start() {
+  return (
+    <>
+        <img href={nfl} alt='nfl'></img>
+        <h1>Welcome to the boys superbowl predictions!</h1>
+    </>
+  )
+}
